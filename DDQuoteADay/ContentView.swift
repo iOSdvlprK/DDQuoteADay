de42a5848664a412fc7045acc01bcb6266a5a2eb
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-08-28 22:08:44 +0900

    Initial Commit

---
+++ DDQuoteADay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DDQuoteADay
+//
+//  Created by joe on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDQuoteADay/DDQuoteADayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDQuoteADayApp.swift
+//  DDQuoteADay
+//
+//  Created by joe on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DDQuoteADayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
